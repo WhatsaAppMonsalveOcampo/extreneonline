1c58c83c872068da958b1e236713a9788462338e
Daniel Botero <danes.daesboar02@gmail.com>
2023-09-18 15:33:51 -0500

    vamos bien

--- public/index.js
+++ public/index.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";var swiper=new Swiper(".mySwiper",{slidesPerView:1,spaceBetween:30,loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
