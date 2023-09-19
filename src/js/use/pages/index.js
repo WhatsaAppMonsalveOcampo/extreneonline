2560bf988206731edc965effe185747a5661e2f8
Daniel Botero <danes.daesboar02@gmail.com>
2023-09-19 18:00:46 -0500

    OK

--- src/js/use/pages/index.js
+++ src/js/use/pages/index.js
@@ -12,3 +12,16 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+var swiper = new Swiper(".slider-hook", {
+    slidesPerView: 1.5,
+    spaceBetween: 24,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
